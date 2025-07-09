416b447dacd77f96cba4bde9966c1935b63f2ed2
Clement Sung <clement.sung7@gmail.com>
2025-07-08 22:10:25 -0400

    update product item page, cart, orders, login

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -5,16 +5,21 @@ import React from 'react'
 
 const CategoryPage = () => {
   return (
-	<div className='flex text-red-500'>
-    {pizzas.map(item=>(
-      <Link className='' href={`/product/${item.id}`} key={item.id}>
+	<div className='flex flex-wrap text-red-500'>
+    {pizzas.map((item) => (
+      <Link className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-1/2 lg:w-1/3 p-4 flex flex-col justify-between group even:bg-fuchsia-50' href={`/product/${item.id}`} key={item.id}>
         {/* Image container */}
         {item.img && (
-          <div>
-            <Image src={item.img} alt=''/>
+          <div className='relative h-[80%]'>
+            <Image src={item.img} alt='' fill className='object-contain'/>
           </div>
         )}
         {/* Text container */}
+        <div className='flex items-center justify-between font-bold '>
+          <h1 className='text-2xl uppercase'>{item.title}</h1>
+          <h2 className='group-hover:hidden'>${item.price}</h2>
+          <button className='hidden group-hover:block uppercase bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
+        </div>
       </Link>
     ))}
   </div>
